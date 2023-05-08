17ccb0b8bc440f7c43556f3c84c858312f722271
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-05-08 18:44:53 -0300

    criei html e css

---
+++ script.js
@@ -0,0 +1,8 @@
+let tar = document.getElementById('tar')
+let resultado = document.getElementById('res')
+let listaTarefas = []
+
+function adicionar() {
+    listaTarefas.push(tar.value)
+    let tarefas = document.createElement('div')
+}
