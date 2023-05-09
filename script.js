ca458f3976f42f28e9add7e97b0303d14c398bf8
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-05-09 10:17:24 -0300

    update js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let tar = document.getElementById('tar')
-let resultado = document.getElementById('res')
+let resultado = document.querySelector('ul')
 let listaTarefas = []
 
 function adicionar() {
-    listaTarefas.push(tar.value)
-    let tarefas = document.createElement('div')
+    listaTarefas.push(tar)
+    let tarefa = document.createElement('li')
+    tarefa.innerHTML = tar.value
+    resultado.appendChild(tarefa)
+    tar.value = ''
+    tar.focus()
 }
